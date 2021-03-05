124cebfb0e55672867bc17403b597af6e87381bc
Alexandra Allen <alexandra.allen1@g.austincc.edu>
2021-03-04 20:40:53 -0600

    homework 4

--- src/classwork/04_assign/loops.cpp
+++ src/classwork/04_assign/loops.cpp
@@ -1,1 +1,15 @@
 //cpp
+#include "loops.h"
+
+int factorial(int num)
+{
+    int total = 1;
+    int count = 1;
+
+    while(count <= num)
+    {
+        total = total * count++;
+    }
+
+    return total;
+}
